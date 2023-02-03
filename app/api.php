4497eb4c35e4a73463414b28467fea76b7f761c6
Arthur Lacoste <arthurlacoste@users.noreply.github.com>
2023-02-03 22:14:18 +0100

    Remove unnecessary error log

--- app/api.php
+++ app/api.php
@@ -2,14 +2,9 @@
 
 use App\Controller\PaypalController;
 
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
-
 header('Content-Type: application/json');
 
 require 'config.php';
-
-
 require '../src/PaypalController.php';
 
 $paypal = new PaypalController();
